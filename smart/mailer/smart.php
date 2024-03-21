6a6b44560a86cb52d5a4fd806f9321b4728a3f63
Vera <158355396+VeraMalygina@users.noreply.github.com>
2024-03-21 11:58:25 +0100

    Update smart.php

--- smart/mailer/smart.php
+++ smart/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'veramalygina0@gmail.com';                 // Наш логин
-$mail->Password = '19141914A';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 3520;                                    // TCP port to connect to
  
-$mail->setFrom('veramalygina0@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('malygina.vera95@bk.ru');     // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
